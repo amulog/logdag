491d6500f3b02000d9de8bc0cbdf06f6676999d1
sat-nii-mbp <sat@nii.ac.jp>
2018-07-31 16:48:14 +0900

    use lingam fast algorithm option

--- logdag/lingam_input.py
+++ logdag/lingam_input.py
@@ -15,7 +15,8 @@ from lingam_fast import lingam_fast
 def estimate(data):
     matrix = pd.DataFrame(data)
     lingam = lingam_fast.LiNGAM()
-    ret = lingam.fit(matrix, use_sklearn = True, reg_type = "lasso")
+    ret = lingam.fit(matrix, use_sklearn = True, algorithm="fast",
+                     reg_type = "lasso")
     graph = lingam.visualize(lib = "networkx")
     return graph
 
