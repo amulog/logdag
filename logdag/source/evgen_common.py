06559ca54c40724c331339b3410fc98b19a2c8e2
sat <sat@nii.ac.jp>
2019-07-17 19:11:39 +0900

    move evgen_common

---
+++ logdag/source/evgen_common.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+source = ["log", "snmp"]
+
+class EventLoader(object):
+    fields = []
+
+    def __init__(self, conf, dry = False):
+        raise NotImplementedError
+
+    def read_all(self):
+        raise NotImplementedError
+
+    def read(self):
+        raise NotImplementedError
+
+    def dump(self):
+        raise NotImplementedError
+
+    def all_feature(self):
+        raise NotImplementedError
+
+    def all_condition(self, dt_range = None):
+        for featurename in self.all_feature():
+            measure = featurename
+            if dt_range is None:
+                l_d_tags = self.evdb.list_series(measure = measure)
+            else:
+                ut_range = tuple(dt.timestamp() for dt in dt_range)
+                l_d_tags = self.evdb.list_series(measure = measure,
+                                                 ut_range = ut_range)
+            for d_tags in l_d_tags:
+                yield (measure, d_tags["host"], int(d_tags["key"]))
+
+    def load(self, measure, host, key, dt_range, binsize):
+        d_tags = {"host": host, "key": key}
+        ut_range = tuple(dt.timestamp() for dt in dt_range)
+        str_bin = config.dur2str(binsize)
+        return self.evdb.get_df(measure, d_tags, self.fields,
+                                ut_range, str_bin, func = "sum", fill = 0)
+
+    def load_items(self, measure, host, key, dt_range):
+        d_tags = {"host": host, "key": key}
+        ut_range = tuple(dt.timestamp() for dt in dt_range)
+        return self.evdb.get_items(measure, d_tags, self.fields, ut_range)
+
+    def load_all(self, dt_range, binsize):
+        for featurename in self.all_feature:
+            measure = featurename
+            for series in self.all_series(measure):
+                host, key = tuple(series)
+                df = self.load(measure, host, key, dt_range, binsize)
+                yield (host, key, df)
+
+    def drop_feature(self):
+        for measure in self.all_feature():
+            if not self.dry:
+                self.evdb.drop_measure(measure)
+            print(measure)
+
+
