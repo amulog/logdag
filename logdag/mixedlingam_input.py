9fc9476e166fd89fa77526ced21ee48161c76dbb
sat <sat@nii.ac.jp>
2020-05-08 18:08:38 +0900

    add a initialized script

---
+++ logdag/mixedlingam_input.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import logging
+import numpy as np
+import networkx as nx
+
+_logger = logging.getLogger(__package__)
+
+
+def estimate(data, skel_th, ci_func, skel_method, pc_depth,
+             skel_verbose, init_graph):
+    import pcalg
+    from gsq.ci_tests import ci_test_bin
+
+    pc_args = {"indep_test_func": ci_test_bin,
+               "data_matrix": data.values,
+               "alpha": skel_th,
+               "method": skel_method,
+               "verbose": skel_verbose}
+    if pc_depth is not None and pc_depth >= 0:
+        args["max_reach"] = pc_depth
+    if init_graph is not None:
+        args["init_graph"] = init_graph
+    (g, sep_set) = pcalg.estimate_skeleton(**args)
+
+    # TODO: to replace something with mixedlingam
+    g = pcalg.estimate_cpdag(skel_graph=g, sep_set=sep_set)
+    # end
+
+    return g
+
+
+def pc_fisherz(data, threshold, skel_method, pc_depth=None,
+
+
+
+    lingam = lingam_fast.LiNGAM()
+    ret = lingam.fit(data, use_sklearn = True, algorithm="fast",
+                     reg_type = "lasso")
+    graph = lingam.visualize(lib = "networkx")
+    return graph
+
