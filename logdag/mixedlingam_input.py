eb7882fe6db8958b266267ca048dcc086d7ec205
Richard Jarry <jarryri@dns-analy4>
2020-06-03 00:26:57 +0900

    Removed estimate_cpdag preprocessing + changed naming convention for prune/normalize

--- logdag/mixedlingam_input.py
+++ logdag/mixedlingam_input.py
@@ -26,12 +26,10 @@ def estimate(data, skel_th, ci_func, skel_method, pc_depth, skel_verbose, init_g
         pc_args["max_reach"] = pc_depth
     if init_graph is not None:
         pc_args["init_graph"] = init_graph
-    (g, sep_set) = pcalg.estimate_skeleton(**pc_args)
 
-    # TODO: MixedLiNGAM integration (select function)
-    graph = MixedGraph(pcalg.estimate_cpdag(skel_graph=g, sep_set=sep_set))
+    (graph, sep_set) = pcalg.estimate_skeleton(**pc_args)
     mapping = {k: v for k, v in zip(graph.nodes(), data.columns.astype(int))}
-    graph = nx.relabel_nodes(graph, mapping)
+    graph = MixedGraph(nx.relabel_nodes(graph, mapping))
     subgraphs = [
         graph.subgraph(nodes)
         for nodes in nx.weakly_connected_components(graph)
@@ -41,7 +39,7 @@ def estimate(data, skel_th, ci_func, skel_method, pc_depth, skel_verbose, init_g
     for sub in subgraphs:
         graph_n = MixedGraph(sub)
         data_n = data[data.columns[graph_n.nodes()]]
-        graph_n, data_n, mapping_n = prune(graph_n, data_n)
+        graph_n, data_n, mapping_n = normalize(graph_n, data_n)
         graph_n = select(graph_n, data_n)
         invmap_n = {v: k for k, v in mapping_n.items()}
         graph_n = nx.relabel_nodes(graph_n, invmap_n)
@@ -50,7 +48,7 @@ def estimate(data, skel_th, ci_func, skel_method, pc_depth, skel_verbose, init_g
     return graph_final
 
 
-def prune(graph: MixedGraph, data):
+def normalize(graph: MixedGraph, data):
     lone = list(nx.isolates(graph))
     graph.remove_nodes_from(lone)
     mapping = dict(zip(graph.nodes(), range(len(graph.nodes()))))
@@ -59,24 +57,3 @@ def prune(graph: MixedGraph, data):
     data.columns = [str(n) for n in graph.nodes()]
     return (graph, data, mapping)
 
-
-def adjust(data, graph: MixedGraph, subgraph):
-    d = data
-    g = graph.copy()
-    c = 0
-    for N in graph.nodes():
-        if N not in subgraph:
-            g.remove_node(N)
-            d = d.drop(d.columns[N - c], axis=1)
-            c += 1
-    m = dict(zip(g.nodes(), range(len(g.nodes()))))
-    g = nx.relabel_nodes(g, m)
-    return d, g, m
-
-
-def relabel(graph: MixedGraph, map1, map2) -> MixedGraph:
-    inv_map1 = {v: k for k, v in map1.items()}
-    inv_map2 = {v: k for k, v in map2.items()}
-    m = {k: inv_map1[inv_map2[k]] for k in graph.nodes()}
-    g = nx.relabel_nodes(graph, m)
-    return g
